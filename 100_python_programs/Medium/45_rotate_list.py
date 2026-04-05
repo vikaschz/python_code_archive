dc4da610dd471d1f038be195385832ff47ff7fd8
vikas channaveerashettar <vikasch5623@gmail.com>
2026-04-06 00:28:43 +0530

    updated code

--- 100_python_programs/Medium/45_rotate_list.py
+++ 100_python_programs/Medium/45_rotate_list.py
@@ -1,17 +1,16 @@
 """
 Program: Rotate List
-Description: Rotate list right by k positions.
-
-Hint:
-- Use slicing or modular indexing.
-
+Description: Rotate list right and left by k positions.
 """
+
 k = int(input("Enter k (number of rotations): "))
-lst = [2, 4, 6, 8, 10]
+
+lst = [1, 2, 3, 4, 5, 6, 7]
 k = k % len(lst)
 
-
 rotated_list = lst[-k:] + lst[:-k]
-print(rotated_list)
+print(f"right shift: {rotated_list}")
 
 
+rotated_list = lst[k:] + lst[:k]
+print(f"left shift: {rotated_list}")
