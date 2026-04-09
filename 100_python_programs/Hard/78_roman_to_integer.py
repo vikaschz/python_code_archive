cf4d6ea936f6af1ccf5c890e13a5820bb619ff1d
vikas channaveerashettar <vikasch5623@gmail.com>
2026-04-09 23:43:24 +0530

    Add Roman to Integer solution

--- 100_python_programs/Hard/78_roman_to_integer.py
+++ 100_python_programs/Hard/78_roman_to_integer.py
@@ -7,24 +7,38 @@ Hint:
 
 """
 
-values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-
-number = input("Enter number in roman numerals: ")
 result = 0
-i = 0
 
-while i < len(number):
-
-    # Try 2-character match first
-    if i + 1 < len(number) and number[i:i+2] in symbols:
-        idx = symbols.index(number[i:i+2])
-        result += values[idx]
-        i += 2
+def roman(c):
+    if c == "I":
+        return 1
+    elif c == "V":
+        return 5
+    elif c == "X":
+        return 10
+    elif c == "L":
+        return 50
+    elif c == "C":
+        return 100
+    elif c == "D":
+        return 500
+    elif c == "M":
+        return 1000
     else:
-        # single character
-        idx = symbols.index(number[i])
-        result += values[idx]
-        i += 1
+        return 0
+
+
+roman_int = input("Enter roman integer: ")
+
+for i in range(len(roman_int)):
+    current = roman(roman_int[i])
+    if i < len(roman_int) - 1:
+        nex_t = roman(roman_int[i + 1])
+        if current < nex_t:
+            result -= current
+        else:
+            result += current
+    else:
+        result += current
 
 print(result)
